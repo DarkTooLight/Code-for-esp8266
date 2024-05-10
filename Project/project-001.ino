2996fd578a840c71eaf5482716fa3fc1255a9083
DarkTooLight <naymur0rahman@gmail.com>
2024-05-10 14:30:22 +0600

    update project-001.ino

---
+++ Project/project-001.ino
@@ -0,0 +1,11 @@
+// when i plss a button the Led goes to lighting  and i plss agin the Led goes to off
+
+int Led = D0;
+int Button = D2;
+
+
+void setup(){
+  pinMode(Led, OUTPUT);
+  pinMode(Button, INPUT);
+  
+}
